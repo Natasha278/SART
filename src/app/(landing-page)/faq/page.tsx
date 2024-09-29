7ea9bcb89846341a49a8769531151bbf1a620bad
natasha <natasha.hernan.p@gmail.com>
2024-09-29 11:31:18 +0200

    ..

--- src/app/(landing-page)/faq/page.tsx
+++ src/app/(landing-page)/faq/page.tsx
@@ -1,3 +1,10 @@
 export default function Faq() {
-  return <div> Hello</div>;
+  return (
+    <div className="flex h-screen justify-center bg-slate-50 pt-10">
+      {" "}
+      <div className="flex w-full max-w-7xl justify-center px-10 text-slate-950">
+        <span>FAQ</span>
+      </div>
+    </div>
+  );
 }
